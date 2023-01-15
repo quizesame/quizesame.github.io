ff6ac4d6d1f250dc640e1643597f48279a44b9aa
Leonardo <leodelfo.ld@gmail.com>
2023-01-15 14:08:45 +0100

    saves each answer in a map

--- js/script.js
+++ js/script.js
@@ -1,36 +1,77 @@
-var quest_n = 1;
-var map = {};
+var quest_curr;
+var quest_next;
+var data;
+var map = new Object();
 
-function setup() {
+function setup(val) {
+    quest_curr = val;
+    quest_next = quest_curr + 1;
+
+    // disable prev or next buttons
+    if (quest_curr == 1) {
+        document.getElementById("prev").setAttribute("disabled", "");
+    }
+    else if (quest_next == 70) {
+        document.getElementById("next").setAttribute("disabled", "");
+    }
+    else {
+        document.getElementById("prev").removeAttribute("disabled", "");
+        document.getElementById("next").removeAttribute("disabled", "");
+    }
+
+    // check if the answer has already been given 
+    if (map[quest_curr.toString()] == "T" || map[quest_curr.toString()] == "F") {
+        document.getElementById(map[quest_curr.toString()] == "T" ? "true" : "false").classList.add("active");
+    }
+    if (map[quest_next.toString()] == "T" || map[quest_next.toString()] == "F") {
+        document.getElementById(map[quest_next.toString()] == "T" ? "true2" : "false2").classList.add("active");
+    }
+
     $.getJSON("https://raw.githubusercontent.com/quizanatomia/quizanatomia.github.io/main/domande.json", function(data) {
-        document.getElementById("quest_n").innerHTML = "Domanda " + quest_n;
-        document.getElementById("quest").innerHTML = data[0]["question"];
+        document.getElementById("quest_n").innerHTML = "Domanda " + quest_curr;
+        document.getElementById("quest").innerHTML = data[quest_curr-1]["question"];
 
-        document.getElementById("quest_n2").innerHTML = "Domanda " + (quest_n+1);
-        document.getElementById("quest2").innerHTML = data[1]["question"];
+        document.getElementById("quest_n2").innerHTML = "Domanda " + (quest_next);
+        document.getElementById("quest2").innerHTML = data[quest_curr]["question"];
     });
 }
 
 function answerTrue() {
     document.getElementById("false").classList.remove("active");
     document.getElementById("true").classList.add("active");
-    map[quest_n.toString()] = "T";
+    map[quest_curr.toString()] = "T";
 }
 
 function answerFalse() {
     document.getElementById("true").classList.remove("active");
     document.getElementById("false").classList.add("active");
-    map[quest_n.toString()] = "F";
+    map[quest_curr.toString()] = "F";
 }
 
 function answerTrue2() {
     document.getElementById("false2").classList.remove("active");
     document.getElementById("true2").classList.add("active");
-    map[(quest_n+1).toString()] = "T";
+    map[quest_next.toString()] = "T";
 }
 
 function answerFalse2() {
     document.getElementById("true2").classList.remove("active");
     document.getElementById("false2").classList.add("active");
-    map[(quest_n+1).toString()] = "F";
+    map[quest_next.toString()] = "F";
+}
+
+function nextQuestion() {
+    document.getElementById("true").classList.remove("active");
+    document.getElementById("false").classList.remove("active");
+    document.getElementById("true2").classList.remove("active");
+    document.getElementById("false2").classList.remove("active");
+    setup(quest_curr+2);
+}
+
+function prevQuestion() {
+    document.getElementById("true").classList.remove("active");
+    document.getElementById("false").classList.remove("active");
+    document.getElementById("true2").classList.remove("active");
+    document.getElementById("false2").classList.remove("active");
+    setup(quest_curr-2);
 }
