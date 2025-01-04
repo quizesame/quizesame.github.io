50be84005ba980e8d1b763f21f4dd04caca649c1
quizesame <33761937+quizesame@users.noreply.github.com>
2025-01-04 17:37:19 +0100

    Update timer.js

--- js/timer.js
+++ js/timer.js
@@ -1,85 +1,26 @@
-var targetObj = {};
-var targetProxy = new Proxy(targetObj, {
-  set: function (target, key, value) {
-	  if (value == -1) {
-		this.running = false;
-		expired = 1;
-		setPoints();
-	  }
-      target[key] = value;
-      return true;
-  }
-});
-targetProxy.diff = 0;
+var countdown;
+var totalTime = 1800; // 30 minutes
 
-function CountDownTimer(duration, granularity) {
-	this.duration = duration;
-	this.granularity = granularity || 1000;
-	this.tickFtns = [];
-	this.running = false;
-}
+function startTimer() {
+  var display = document.getElementById("timer");
+  display.style.display = "block"; // show it when quiz starts
+  var start = Date.now();
 
-CountDownTimer.prototype.start = function () {
-	if (this.running) {
-		return;
-	}
-	this.running = true;
-	var start = Date.now(),
-		that = this,
-		obj;
+  countdown = setInterval(function () {
+    var elapsed = Math.floor((Date.now() - start) / 1000);
+    var remaining = totalTime - elapsed;
 
-	(function timer() {
-		targetProxy.diff = that.duration - (((Date.now() - start) / 1000) | 0);
-
-		if (expired) {
-			targetProxy.diff = -1;
-		}
-
-		// console.log(targetProxy.diff)
-		if (targetProxy.diff >= 0) {
-			setTimeout(timer, that.granularity);
-		} else {
-			targetProxy.diff = 0;
-			that.running = false;
-			return;
-		}
-
-		obj = CountDownTimer.parse(targetProxy.diff);
-		that.tickFtns.forEach(function (ftn) {
-			ftn.call(this, obj.minutes, obj.seconds);
-		}, that);
-	}());
-};
-
-CountDownTimer.prototype.onTick = function (ftn) {
-	if (typeof ftn === 'function') {
-		this.tickFtns.push(ftn);
-	}
-	return this;
-};
-
-CountDownTimer.prototype.expired = function () {
-	return !this.running;
-};
-
-CountDownTimer.parse = function (seconds) {
-	return {
-		'minutes': (seconds / 60) | 0,
-		'seconds': (seconds % 60) | 0
-	};
-};
-
-function start() {
-    var display = document.querySelector("#timer"),
-        timer = new CountDownTimer(1500);
-
-    timer.onTick(format(display)).start();
-
-    function format(display) {
-        return function (minutes, seconds) {
-            minutes = minutes < 10 ? "0" + minutes : minutes;
-            seconds = seconds < 10 ? "0" + seconds : seconds;
-            display.textContent = minutes + ' : ' + seconds;
-        };
+    if (remaining <= 0) {
+      clearInterval(countdown);
+      finishQuiz(); // auto-finish if time expires
+      display.textContent = "00 : 00";
+      return;
     }
-};
+
+    var minutes = Math.floor(remaining / 60);
+    var seconds = remaining % 60;
+    if (minutes < 10) minutes = "0" + minutes;
+    if (seconds < 10) seconds = "0" + seconds;
+    display.textContent = minutes + " : " + seconds;
+  }, 1000);
+}
